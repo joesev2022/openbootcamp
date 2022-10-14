4bdb0cf9505c03ecda2d72ccdf6509b0e0b5eb5f
Jose_Apaza <jose.apazasev@gmail.com>
2022-10-14 06:35:03 -0500

    Ejercicio 01 - Java Básico

---
+++ java/Obc_java_ejercicio_01/src/clases/App.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        long id = 8495L;
+        int celular = 999165235;
+        double presupuesto = 1000.50;
+        boolean isHombre = true;
+        String nombre = "Roberto Carlos Sanchez";
+        System.out.println("id: " + id + ", nombre: " + nombre + ", celular: " + celular 
+                            + ", presupuesto: " + presupuesto
+                            + ", Sexo: " + isHombre);
+    }
+}
