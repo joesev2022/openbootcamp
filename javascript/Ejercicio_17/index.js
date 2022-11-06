f84b644428914710c4f5113c4e1bcae51455582f
Jose_Apaza <jose.apazasev@gmail.com>
2022-11-06 11:47:55 -0500

    This is the seventeenth exercise in javascript at Open Bootcamp

---
+++ javascript/Ejercicio_17/index.js
@@ -0,0 +1,34 @@
+// Initialize and add the map
+function initMap() {
+    var locations = [
+        ['Bondi Beach', -33.890542, 151.274856, 4],
+        ['Coogee Beach', -33.923036, 151.259052, 5],
+        ['Cronulla Beach', -34.028249, 151.157507, 3],
+    ];
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: new google.maps.LatLng(-33.92, 151.25),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+    
+    for (i = 0; i < locations.length; i++) {  
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+    });
+    
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+        }
+    })(marker, i));
+    }
+}
+
+window.initMap = initMap;
